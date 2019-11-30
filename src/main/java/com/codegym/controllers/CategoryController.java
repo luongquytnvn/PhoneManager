20fc823fe61a5b9353b3fdeadaf6521a8b40d8b1
luongquytnvn <luongquytnvn@gmail.com>
2019-11-30 08:00:07 +0700

    update

--- src/main/java/com/codegym/controllers/CategoryController.java
+++ src/main/java/com/codegym/controllers/CategoryController.java
@@ -11,6 +11,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,6 +23,10 @@ public class CategoryController {
     CategoryService categoryService;
     @Autowired
     PhoneService phoneService;
+    @ModelAttribute("categories")
+    public Page<Category> categories(Pageable pageable){
+        return categoryService.findAll(pageable);
+    }
     @GetMapping("/category-list")
     public String categoryList(Model model, @PageableDefault(size = 10) Pageable pageable){
         Page<Category> categories = categoryService.findAll(pageable);
